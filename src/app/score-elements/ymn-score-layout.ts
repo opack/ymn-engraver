4cdb804da3cf95dea32a2be89bf9a98b12e05fd5
ddemange <ddemange@easyvista.com>
2017-10-27 17:14:33 +0200

    Add beat bars around tempo value

--- src/app/score-elements/ymn-score-layout.ts
+++ src/app/score-elements/ymn-score-layout.ts
@@ -7,36 +7,44 @@ import { YmnLongNote } from './ymn-long-note';
  * Ensures that the children in one score are correcly laid out one to another
  */
 export class YmnScoreLayout {
-    public layout(score: YmnScore): void {
-        // Layout score information
-        score.shape.title.position({
-            x: (ShapeConfig.score.width - score.shape.title.getClientRect().width) / 2,
-            y: ShapeConfig.score.title.topMargin
-        });
-        score.shape.author.position({
-            x: (ShapeConfig.score.width - score.shape.author.width()) / 2,
-            y: score.shape.title.y() + score.shape.title.height() + ShapeConfig.score.title.bottomSpacing
-        });
-        score.shape.tempo.position({
-            x: ShapeConfig.score.tempo.leftPosition,
-            y: score.shape.author.y() + score.shape.author.height()
-        });
+  public layout(score: YmnScore): void {
+    // Layout score information
+    score.shape.title.position({
+      x: (ShapeConfig.score.width - score.shape.title.getClientRect().width) / 2,
+      y: ShapeConfig.score.title.topMargin
+    });
+    score.shape.author.position({
+      x: (ShapeConfig.score.width - score.shape.author.width()) / 2,
+      y: score.shape.title.y() + score.shape.title.height() + ShapeConfig.score.title.bottomSpacing
+    });
+    score.shape.leftTempoSeparator.position({
+      x: ShapeConfig.score.tempo.leftPosition,
+      y: score.shape.author.y() + score.shape.author.height()
+    });
+    score.shape.tempo.position({
+      x: score.shape.leftTempoSeparator.x() + score.shape.leftTempoSeparator.width() + 5,
+      y: score.shape.leftTempoSeparator.y() + score.shape.leftTempoSeparator.height() / 2 - score.shape.tempo.height() / 2
+    });
+    score.shape.rightTempoSeparator.position({
+      x: score.shape.tempo.x() + score.shape.tempo.width() + 5,
+      y: score.shape.leftTempoSeparator.y()
+    });
 
-        let yOffset = score.shape.tempo.y() + score.shape.tempo.height() + ShapeConfig.score.tempo.bottomSpacing;
-        score.children.forEach(system => {
-            const systemLayout = new YmnSystemLayout();
-            yOffset = systemLayout.layout(system, yOffset);
-        });
+    let yOffset = score.shape.leftTempoSeparator.y() + score.shape.leftTempoSeparator.height() + ShapeConfig.score.tempo.bottomSpacing;
+    score.children.forEach(system => {
+      const systemLayout = new YmnSystemLayout();
+      yOffset = systemLayout.layout(system, yOffset);
+    });
 
-        // Search long notes to draw continuation lines
-        const longNotes: Array<YmnLongNote> = [];
-        score.children.forEach(system => {
-            system.children.forEach(staff => {
-                staff.searchLongNotes().forEach(longNote => {
-                    longNotes.push(longNote);
-                })
-            });
+    // Search long notes to draw continuation lines
+    const longNotes: Array<YmnLongNote> = [];
+    score.children.forEach(system => {
+      system.children.forEach(staff => {
+        staff.searchLongNotes().forEach(longNote => {
+          longNotes.push(longNote);
         });
-        score.shape.drawContinuationLines(longNotes);
-    }
+      });
+    });
+    score.shape.drawContinuationLines(longNotes);
+  }
 }
--- src/app/score-elements/ymn-score-shape.ts
+++ src/app/score-elements/ymn-score-shape.ts
@@ -3,105 +3,122 @@ import { ShapeConfig } from './shape-constants';
 import { YmnLongNote } from './ymn-long-note';
 
 export class YmnScoreShape extends Konva.Layer {
-    public title: Konva.Text;
-    public author: Konva.Text;
-    public tempo: Konva.Text;
-    public continuationLines: Konva.Group;
+  public title: Konva.Text;
+  public author: Konva.Text;
+  public tempo: Konva.Text;
+  public leftTempoSeparator: Konva.Line;
+  public rightTempoSeparator: Konva.Line;
 
-    constructor() {
-        super();
-        
-        this.title = new Konva.Text ({
-            text: 'Title',
-            fontSize: ShapeConfig.score.title.fontSize,
-            fontFamily: ShapeConfig.score.title.fontFamily,
-            align: 'center'
-        });
-        this.add(this.title);
+  public continuationLines: Konva.Group;
 
-        this.author = new Konva.Text ({
-            text: 'Author',
-            fontSize: ShapeConfig.score.author.fontSize,
-            fontFamily: ShapeConfig.score.author.fontFamily,
-            align: 'center'
-        });
-        this.add(this.author);
+  constructor() {
+    super();
 
-        this.tempo = new Konva.Text ({
-            text: 'Tempo',
-            fontSize: ShapeConfig.score.tempo.fontSize,
-            fontFamily: ShapeConfig.score.tempo.fontFamily,
-            align: 'center'
-        });
-        this.add(this.tempo);
+    this.title = new Konva.Text ({
+      text: 'Title',
+      fontSize: ShapeConfig.score.title.fontSize,
+      fontFamily: ShapeConfig.score.title.fontFamily,
+      align: 'center'
+    });
+    this.add(this.title);
 
-        this.continuationLines = new Konva.Group();
-        this.add(this.continuationLines);
-    }
+    this.author = new Konva.Text ({
+      text: 'Author',
+      fontSize: ShapeConfig.score.author.fontSize,
+      fontFamily: ShapeConfig.score.author.fontFamily,
+      align: 'center'
+    });
+    this.add(this.author);
 
-    public update(title: string, author: string, tempo: number) {
-        this.title.text(title);
-        this.author.text(author);
-        this.tempo.text('' + tempo);
-    }
+    this.tempo = new Konva.Text ({
+      text: 'Tempo',
+      fontSize: ShapeConfig.score.tempo.fontSize,
+      fontFamily: ShapeConfig.score.tempo.fontFamily,
+      align: 'center'
+    });
+    this.add(this.tempo);
 
-    public drawContinuationLines(longNotesList: Array<YmnLongNote>): void {
-        longNotesList.forEach(longNote => {
-            //this.drawStraightLine(longNote.staffShape, longNote.firstNoteShape, longNote.lastNoteShape);
-            this.drawQuadraticLine(longNote.firstNoteShape, longNote.lastNoteShape);
-        });
-        // Ensure that continuation lines will be drawn on top of everything else
-        this.continuationLines.setZIndex(100);
-    }
+    this.leftTempoSeparator = new Konva.Line({
+      points: [
+        0, 0,
+        0, ShapeConfig.staff.defaultHeight],
+      stroke: ShapeConfig.beat.stroke,
+      strokeWidth: ShapeConfig.beat.strokeWidth,
+      dash: ShapeConfig.beat.dash
+    });
+    this.add(this.leftTempoSeparator);
 
-    private drawStraightLine(staff: Konva.Node, from: Konva.Node, to: Konva.Node) {
-        //const staffPosition = staff.getAbsolutePosition(); // Only needed if the lines go back to the staff as their position would then not be absolute
-        const fromPosition = from.getAbsolutePosition();
-        const toPosition =  to.getAbsolutePosition();
+    this.rightTempoSeparator = <Konva.Line>this.leftTempoSeparator.clone();
+    this.add(this.rightTempoSeparator);
 
-        this.add(new Konva.Line({
-            points: [
-                fromPosition.x /*- staffPosition.x*/ + from.width() - ShapeConfig.staff.continuationNotesLine.firstNoteInnerMargin, fromPosition.y /*- staffPosition.y*/ + to.height() / 2 - ShapeConfig.stage.yOffset,
-                toPosition.x /*- staffPosition.x*/ + to.width() / 2, toPosition.y /*- staffPosition.y*/ + to.height() / 2 - ShapeConfig.stage.yOffset
-            ],
-            stroke: ShapeConfig.staff.continuationNotesLine.stroke,
-            strokeWidth: ShapeConfig.staff.continuationNotesLine.strokeWidth,
-            dash: ShapeConfig.staff.continuationNotesLine.dash
-        }));
-    }
+    this.continuationLines = new Konva.Group();
+    this.add(this.continuationLines);
+  }
 
-    private drawQuadraticLine(from: Konva.Node, to: Konva.Node) {
-        const fromPosition = from.getAbsolutePosition();
-        const toPosition =  to.getAbsolutePosition();
-        const quad = {
-            start: {
-                x: fromPosition.x + from.width() - ShapeConfig.staff.continuationNotesLine.firstNoteInnerMargin,
-                y: fromPosition.y + from.height() / 2,},
-            control: {
-                // Control will be defined just after start and end have been defined for more clarity
-                x: 0,
-                y: 0
-            },
-            end: {
-                x: toPosition.x +  to.width() / 2,
-                y: toPosition.y +  to.height() / 2}
-        };
-        quad.control = {
-            x: (quad.end.x + quad.start.x) / 2,
-            y: quad.start.y - ShapeConfig.staff.continuationNotesLine.curveHeight
-        };
+  public update(title: string, author: string, tempo: number) {
+    this.title.text(title);
+    this.author.text(author);
+    this.tempo.text('' + tempo);
+  }
 
-        const continuationLine = new Konva.Shape({
-            stroke: ShapeConfig.staff.continuationNotesLine.stroke,
-            strokeWidth: ShapeConfig.staff.continuationNotesLine.strokeWidth,
-            lineCap: 'round',
-            sceneFunc: function(context) {
-                context.beginPath();
-                context.moveTo(quad.start.x, quad.start.y);
-                context.quadraticCurveTo(quad.control.x, quad.control.y, quad.end.x, quad.end.y);
-                context.strokeShape(this);
-            }
-        });
-        this.continuationLines.add(continuationLine);
-    }
+  public drawContinuationLines(longNotesList: Array<YmnLongNote>): void {
+    longNotesList.forEach(longNote => {
+      //this.drawStraightLine(longNote.staffShape, longNote.firstNoteShape, longNote.lastNoteShape);
+      this.drawQuadraticLine(longNote.firstNoteShape, longNote.lastNoteShape);
+    });
+    // Ensure that continuation lines will be drawn on top of everything else
+    this.continuationLines.setZIndex(100);
+  }
+
+  private drawStraightLine(staff: Konva.Node, from: Konva.Node, to: Konva.Node) {
+    //const staffPosition = staff.getAbsolutePosition(); // Only needed if the lines go back to the staff as their position would then not be absolute
+    const fromPosition = from.getAbsolutePosition();
+    const toPosition =  to.getAbsolutePosition();
+
+    this.add(new Konva.Line({
+      points: [
+        fromPosition.x /*- staffPosition.x*/ + from.width() - ShapeConfig.staff.continuationNotesLine.firstNoteInnerMargin, fromPosition.y /*- staffPosition.y*/ + to.height() / 2 - ShapeConfig.stage.yOffset,
+        toPosition.x /*- staffPosition.x*/ + to.width() / 2, toPosition.y /*- staffPosition.y*/ + to.height() / 2 - ShapeConfig.stage.yOffset
+      ],
+      stroke: ShapeConfig.staff.continuationNotesLine.stroke,
+      strokeWidth: ShapeConfig.staff.continuationNotesLine.strokeWidth,
+      dash: ShapeConfig.staff.continuationNotesLine.dash
+    }));
+  }
+
+  private drawQuadraticLine(from: Konva.Node, to: Konva.Node) {
+    const fromPosition = from.getAbsolutePosition();
+    const toPosition =  to.getAbsolutePosition();
+    const quad = {
+      start: {
+        x: fromPosition.x + from.width() - ShapeConfig.staff.continuationNotesLine.firstNoteInnerMargin,
+        y: fromPosition.y + from.height() / 2
+      },
+      control: {
+        // Control will be defined just after start and end have been defined for more clarity
+        x: 0,
+        y: 0
+      },
+      end: {
+        x: toPosition.x +  to.width() / 2,
+        y: toPosition.y +  to.height() / 2}
+    };
+    quad.control = {
+      x: (quad.end.x + quad.start.x) / 2,
+      y: quad.start.y - ShapeConfig.staff.continuationNotesLine.curveHeight
+    };
+
+    const continuationLine = new Konva.Shape({
+      stroke: ShapeConfig.staff.continuationNotesLine.stroke,
+      strokeWidth: ShapeConfig.staff.continuationNotesLine.strokeWidth,
+      lineCap: 'round',
+      sceneFunc: function(context) {
+        context.beginPath();
+        context.moveTo(quad.start.x, quad.start.y);
+        context.quadraticCurveTo(quad.control.x, quad.control.y, quad.end.x, quad.end.y);
+        context.strokeShape(this);
+      }
+    });
+    this.continuationLines.add(continuationLine);
+  }
 }
