2b4f43c8a59fa9a06d68b75dc94b8c3866ceb60b
Opack <github@valdera.fr>
2017-11-14 13:54:25 +0100

    [WIP] Add Firestore support to store scores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import * as FileSaver from 'file-saver';
 
 import { FileUtils } from '../utils/file-utils';
@@ -9,12 +9,27 @@ import { YmnDCRNTranslator } from './ymn-DCRN-translator';
 
 import { TabsetComponent } from 'ngx-bootstrap';
 
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+// TODO Move this out of this file
+interface Post {
+  title: string;
+  content: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public postsCol: AngularFirestoreCollection<Post>;
+  public posts: Observable<Post[]>;
+  public titledbg: string;
+  public content: string;
+
   public title: string;
   public author: string;
   public tempo: string;
@@ -32,6 +47,20 @@ B1| 2.10+7 2..10+7|\n\
 B2|7 ..3 10. 10|';
   @ViewChild('ymnSheet') ymnSheet: YmnSheetComponent;
   @ViewChild('viewTabs') viewTabs: TabsetComponent;
+
+  constructor(private afs: AngularFirestore) {}
+
+  ngOnInit() {
+    this.postsCol = this.afs.collection('posts');
+    this.posts = this.postsCol.valueChanges();
+  }
+
+  addPost() {
+    // Adding a document using a generated ID
+    // this.afs.collection('posts').add({'title': this.titledbg, 'content': this.content});
+    // Adding a document using a custom ID
+    this.afs.collection('posts').doc(this.titledbg + this.content.substr(0, 5)).set({'title': this.titledbg, 'content': this.content});
+  }
 
   public engrave(): void {
     this.ymnSheet.engrave(this.title, this.author, parseInt(this.tempo, 10), this.score);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { YmnSheetComponent } from './ymn-sheet/ymn-sheet.component';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
+// Firebase imports
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,7 +21,16 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
     BrowserModule,
     FormsModule,
     TabsModule.forRoot(),
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    AngularFireModule.initializeApp({
+      apiKey: 'AIzaSyDBjki1FzzfX4vOTQ0G90l3IIn08uV--Wk',
+      authDomain: 'ymn-engraver.firebaseapp.com',
+      databaseURL: 'https://ymn-engraver.firebaseio.com',
+      projectId: 'ymn-engraver',
+      storageBucket: 'ymn-engraver.appspot.com',
+      messagingSenderId: '638871859233'
+    }),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
